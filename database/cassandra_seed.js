f96a679d50e7b31717a59aa0b8d36951b2884a2c
brunningman <abrunner221@gmail.com>
2019-08-26 18:00:01 -0700

    added join table for mysql

--- database/cassandra_seed.js
+++ database/cassandra_seed.js
@@ -1,10 +1,29 @@
 const faker = require('faker');
 const fs = require('fs');
 const promise = require('bluebird');
-const output = __dirname + '/cassandra_seed.cql';
+const output = __dirname + '/cassandra_seed.csv';
 const fsAsync = promise.promisifyAll(fs);
 const exec = require('child_process').exec;
-// const config = require('../config');
+// const createCSVWriter = require('csv-writer').createObjectCsvWriter;
+// const csvWriter =  createCSVWriter({
+//   path: output,
+//   // id: i,
+//   // name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+//   // content: `${faker.lorem.paragraphs(randomIntFromOne(3))}`,
+//   // referenceItem: randomIntFromOne(MAX_ITEMS),
+//   // avatar: `${faker.image.people(50, 50)}`,
+//   // createdAt: `${createdAt}`,
+//   // updatedAt: `${createdAt}`
+//   header: [
+//     {id: 'id', title: 'ID'},
+//     {id: 'name', title: 'Name'},
+//     {id: 'content', title: 'Content'},
+//     {id: 'referenceItem', title: 'ReferenceItem'},
+//     {id: 'avatar', title: 'Avatar'},
+//     {id: 'createdAt', title: 'CreatedAt'},
+//     {id: 'updatedAt', title: 'UpdatedAt'}
+//   ]
+// })
 
 const MAX_REVS = 50000000;
 const MAX_ITEMS = 10000000;
@@ -33,43 +52,46 @@ let createRandomDate = () => {
   return `${date.year}-${date.month}-${date.day} ${date.hour}:${date.min}:${date.sec}`
 }
 
-fsAsync.writeFileAsync(output, 'USE bnb; TRUNCATE bnb.reviews;\n')
+fs.writeFileAsync(output, 'id,name,content,referenceitem,avatar,createdat,updatedat')
   .then(() => {
-    console.log('Data gen started @:', Date.now());
+    let start = Date.now();
     // create 50,000,000 reviews
     let query = '';
     for (let i = 1; i <= MAX_REVS; i++) {
       let createdAt = createRandomDate();
       let review = {
         id: i,
-        name: `$$${faker.name.firstName()} ${faker.name.lastName()}$$`,
-        content: `$$${faker.lorem.paragraphs(randomIntFromOne(3))}$$`,
+        name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+        content: `${faker.lorem.sentences(randomIntFromOne(3))}`,
         referenceItem: randomIntFromOne(MAX_ITEMS),
-        avatar: `$$${faker.image.people(50, 50)}$$`,
-        createdAt: `$$${createdAt}$$`,
-        updatedAt: `$$${createdAt}$$`
+        avatar: `${faker.image.people(50, 50)}`,
+        createdAt: `${createdAt}`,
+        updatedAt: `${createdAt}`
       };
 
-      query += `INSERT INTO bnb.reviews (${Object.keys(review).join(',')}) VALUES (${Object.values(review).join(',')});\n`;
+      query += `${review.id},${review.name},${review.content},${review.referenceItem},${review.avatar},${review.createdAt},${review.updatedAt}` + '\n';
       if(i % 100000 === 0) {
         fs.appendFileSync(output, query);
         query = '';
         console.log(`Review ${i}/${MAX_REVS} generated`);
       }
     }
-
+    return start;
   })
-  .then(() => {
-    console.log('Data gen finished @:', Date.now());
-    console.log('Begin cassandra insertion');
-    exec(`cqlsh < ${__dirname}/cassandra_seed.cql`, (stdout, stderr) => {
-      if (stderr) {
-        console.log(stderr);
-      } else {
-        console.log('Insertion finished @:', Date.now());
-      }
-    });
+  .then((start) => {
+    console.log((Date.now() - start)/1000, 'seconds')
   })
+  // .then(() => {
+  //   console.log('Data gen finished @:', Date.now());
+  //   console.log('Begin cassandra insertion');
+  //   exec(`cqlsh < ${__dirname}/cassandra_seed.cql`, (stdout, stderr) => {
+  //     if (stderr) {
+  //       console.log(stderr);
+  //     } else {
+  //       console.log('Insertion finished @:', Date.now());
+  //     }
+  //   });
+  // })
   .catch(err => {
     console.log(err);
   });
---
+++ database/mysql_items_seed.js
@@ -0,0 +1,55 @@
+const faker = require('faker');
+const fs = require('fs');
+const promise = require('bluebird');
+const output = __dirname + '/mysql_items_seed.sql';
+const fsAsync = promise.promisifyAll(fs);
+const exec = require('child_process').exec;
+// const config = require('../config');
+
+const MAX_REVS = 50000000;
+const MAX_ITEMS = 10000000;
+
+let randomIntFromOne = max => {
+  return Math.ceil(Math.random() * max);
+};
+fsAsync.writeFileAsync(output, 'USE bnb; TRUNCATE item_reviews;\n')
+  .then(() => {
+    let items = {};
+    let reviews = {}
+    // create 50,000,000 reviews
+    const setupQuery = 'INSERT INTO item_reviews (item_id, review_id) VALUES '
+    let query = '';
+    let interim = '';
+    for (let i = 1; i <= MAX_REVS; i++) {
+      let item = {
+        item_id: randomIntFromOne(MAX_ITEMS),
+        review_id: i
+      };
+
+      interim += `(${item.item_id},${item.review_id}),`;
+      if(i % 250 === 0) {
+        query += setupQuery + interim.substr(0, interim.length - 1) + ';\n';
+        interim = '';
+      }
+      if(i % 100000 === 0) {
+        fs.appendFileSync(output, query);
+        interim = '';
+        query = '';
+        console.log(`Item ${i}/${MAX_REVS} generated`);
+      }
+    }
+
+  })
+  .then(() => {
+    console.log('Begin mySQL insertion');
+    exec(`mysql -u root < ${__dirname}/mysql_items_seed.sql`, (stdout, stderr) => {
+      if (stderr) {
+        console.log(stderr);
+      } else {
+        console.log(stdout);
+      }
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
