0f7cfe0f60a3900e77f265ee9c99d898373928fa
brunningman <abrunner221@gmail.com>
2019-08-31 12:18:09 -0700

    added getAllByItemID query

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const cassandra = require('cassandra-driver');
 const client = new cassandra.Client({
-  contactPoints: ['h1', 'h2'],
+  contactPoints: ['127.0.0.1'],
   localDataCenter: 'datacenter1',
   keyspace: 'bnb'
 });
@@ -9,3 +9,18 @@ client.connect(err => {
   if(err) throw new Error(err);
 });
 
+const getAllByItemID = itemID => {
+  const query =  `SELECT * FROM bnb.reviews WHERE referenceitem = ${itemID};`;
+  return client.execute(query)
+    .then(data => {
+      console.log(data);
+      return data.rows;
+    })
+    .catch(err => {
+      throw new Error(err);
+    })
+}
+
+module.exports = {
+  getAllByItemID: getAllByItemID
+}
